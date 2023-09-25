cc4942fd55787f4e74f7c09aa998a32cf1d461cb
Do Duc Manh <128243872+CBNPrinze@users.noreply.github.com>
2023-09-25 16:08:27 +0700

    Update HelloWorld.java

--- Homework01/20200383-DoDucManh/HelloWorld.java
+++ Homework01/20200383-DoDucManh/HelloWorld.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user to enter their name
+        System.out.print("Enter your name: ");
+
+        // Read the user's input as a String
+        String name = scanner.nextLine();
+
+        // Display a greeting
+        System.out.println("Hello, " + name + "!");
+
+        // Close the scanner to release resources
+        scanner.close();
+    }
+}
