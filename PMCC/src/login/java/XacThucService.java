bcfb953d464ccefe7548bccbda0d63a6df6c66e0
CBNPrince <104844420+CBNPrince@users.noreply.github.com>
2023-12-20 18:25:56 +0700

    Committing my changes before pulling

--- PMCC/src/login/java/XacThucService.java
+++ PMCC/src/login/java/XacThucService.java
@@ -2,15 +2,15 @@ package login.java;
 
 public class XacThucService {
     protected static String xacThuc(String tenDangNhap, String matKhau){
-        if (tenDangNhap.equals("CongNhan") && matKhau.equals("123456")) {
+        if (tenDangNhap.equals("CN") && matKhau.equals("123456")) {
             return "CongNhan";
-        } else if(tenDangNhap.equals("NhanVienVanPhong") && matKhau.equals("123456"))
+        } else if(tenDangNhap.equals("NVVP") && matKhau.equals("123456"))
             return "NhanVienVanPhong";
-        else if(tenDangNhap.equals("TruongDonViCN") && matKhau.equals("123456"))
+        else if(tenDangNhap.equals("TCN") && matKhau.equals("123456"))
             return "TruongDonViCN";
-        else if(tenDangNhap.equals("TruongDonViNVVP") && matKhau.equals("123456"))
+        else if(tenDangNhap.equals("TNVVP") && matKhau.equals("123456"))
             return "TruongDonViNVVP";
-        else if(tenDangNhap.equals("TruongNhanSu") && matKhau.equals("123456"))
+        else if(tenDangNhap.equals("TNS") && matKhau.equals("123456"))
             return "TruongNhanSu";
         else
             return "";
