d0feb188e08435be7d56bea988cb79c023a99b19
pdt <phamduytung2002@gmail.com>
2023-12-27 04:56:13 +0700

    upload

---
+++ Final/Group/Readme.txt
@@ -0,0 +1,18 @@
+Phân công công việc nhóm 2
+Thành viên:
+	Phạm Duy Tùng
+	Trương Đăng Biển
+	Đỗ Minh Đức
+	Đỗ Đức Mạnh
+
+Phân công các công việc chung:
+	thiết kế database: Đức
+	thiết kế wireframe và sơ đồ chuyển đổi màn hình: Mạnh
+	tổng hợp sơ đồ thiết kế: Biển
+	xây dựng khung chương trình: Tùng
+
+Tỷ lệ đóng góp của các thành viên:
+	Phạm Duy Tùng: 25%
+	Trương Đăng Biển: 25%
+	Đỗ Minh Đức: 25%
+	Đỗ Đức Mạnh: 25%
--- PMCC/src/DBConnector/DBBanGhiChamCong.java
+++ PMCC/src/DBConnector/DBBanGhiChamCong.java
@@ -40,17 +40,24 @@ public class DBBanGhiChamCong extends DBConnector{
         this.insertData((List<Document>) document);
     }
 
+    public void insertOne(String ID, Date thoiGian){
+        Document document = new Document("ID", ID)
+                .append("Thời gian", thoiGian);
+        this.collection.insertOne(document);
+    }
+
+
     public static void main(String[] args) {
         DBBanGhiChamCong dbBanGhiChamCong = new DBBanGhiChamCong();
 
-//        LocalDateTime date1 = LocalDateTime.of(2023, 12, 1, 7, 45);
-//        LocalDateTime date2 = LocalDateTime.of(2023, 12, 1, 11, 30);
-//        LocalDateTime date3 = LocalDateTime.of(2023, 12, 1, 12, 45);
-//        LocalDateTime date4 = LocalDateTime.of(2023, 12, 1, 17, 30);
-//        dbBanGhiChamCong.insertData("20200573", Date.from(date1.atZone(ZoneId.systemDefault()).toInstant()));
-//        dbBanGhiChamCong.insertData("20200573", Date.from(date2.atZone(ZoneId.systemDefault()).toInstant()));
-//        dbBanGhiChamCong.insertData("20200573", Date.from(date3.atZone(ZoneId.systemDefault()).toInstant()));
-//        dbBanGhiChamCong.insertData("20200573", Date.from(date4.atZone(ZoneId.systemDefault()).toInstant()));
+        LocalDateTime date1 = LocalDateTime.of(2023, 11, 2, 7, 43);
+        LocalDateTime date2 = LocalDateTime.of(2023, 11, 2, 11, 35);
+        LocalDateTime date3 = LocalDateTime.of(2023, 11, 1, 12, 47);
+        LocalDateTime date4 = LocalDateTime.of(2023, 11, 1, 17, 31);
+        dbBanGhiChamCong.insertData("20200063", Date.from(date1.atZone(ZoneId.systemDefault()).toInstant()));
+        dbBanGhiChamCong.insertData("20200063", Date.from(date2.atZone(ZoneId.systemDefault()).toInstant()));
+        dbBanGhiChamCong.insertData("20200063", Date.from(date3.atZone(ZoneId.systemDefault()).toInstant()));
+        dbBanGhiChamCong.insertData("20200063", Date.from(date4.atZone(ZoneId.systemDefault()).toInstant()));
         dbBanGhiChamCong.getData();
 //        List<BanGhiChamCong> x = dbBanGhiChamCong.queryByID("20200573");
 //        for(BanGhiChamCong t: x) {
